ad53383cb1d5c26e650803900982c488cb8842b8
Clay Morton <lyonsclay@yahoo.com>
2014-01-16 13:47:48 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Skunkwerx::Application.config.secret_key_base = 'cf566a92ba2629ac6723b3d80e5eeb95f332be3abdc997f8a33dcbad3ddcc5e02984e9cf6f4f32b9bc17a0d12cf2a924a07c9c559a03570395fe84f750934199'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Skunkwerx::Application.config.session_store :cookie_store, key: '_skunkwerx_session'
