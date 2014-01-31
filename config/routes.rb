d474fb69d0f49571473811231164065667dc66a9
Clay Morton <lyonsclay@yahoo.com>
2014-01-31 00:28:31 -0500

    Add contact page with styles and route. Add jQuery google map calls for embedded search function

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Skunkwerx::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  # get 'contact/index'
+
+  resources :contact, only: [:index]
+
+  get 'contact/map'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
