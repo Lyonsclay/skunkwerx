ba3bf59bdafd78233be41692ac2036dbb58af7f5
Clay Morton <lyonsclay@yahoo.com>
2014-02-20 20:40:36 -0500

    ApplicationController include SessionHelper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  # By default helpers are available in views but not in controllers.
+  include SessionsHelper
 end
