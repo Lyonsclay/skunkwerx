61091c8f6408a79a79c185b461444544875f662a
Clay Morton <lyonsclay@yahoo.com>
2014-02-22 00:28:19 -0500

    Password resets feature in progress

---
+++ spec/requests/password_resets_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe "PasswordResets" do
+  it "emails user when requestiong password reset" do
+    admin = create(:admin)
+    visit login_path
+    click_link "password"
+    fill_in "Email", :with => admin.email
+    click_button "Send Email"
+  end
+end
