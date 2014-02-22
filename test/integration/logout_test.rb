eac06e646f3d509e51908bc74aaec574b1f923c1
Clay Morton <lyonsclay@yahoo.com>
2014-02-22 00:30:06 -0500

    Logout integration test BDD start

---
+++ test/integration/logout_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+class LoginTest < ActionDispatch::IntegrationTest
+  fixtures :admin
+
+  test "visit '/admin' page and login if no sessions current" do
+    admin = login(:admin_one)
+
+    admin.visits_site
+
+  end
+
+  private
+
+    module CustomDsl
+      def visits_site
+        get_via_redirect "/admin"
+        assert_response :success
+        assert_equal '/admin/login', path
+      end
+    end
+
+    def login(admin)
+      open_session do |sess|
+        # Add methods to sess from module CustomDsl as singleton methods.
+        sess.extend(CustomDsl)
+        u = admin(admin)
+        sess.https!
+        sess.post "/sessions", session: { username: u.email, password: "foobar" }
+binding.pry
+        sess.assert_response :success
+        sess.https!(false)
+      end
+    end
+end
