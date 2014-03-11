8ab43f1855d5994180594da0348c9ea3671ade94
Clay Morton <lyonsclay@yahoo.com>
2014-03-10 23:04:55 -0400

    Put config.action_mailer in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Skunkwerx::Application.initialize!
 
+config.action_mailer.default_url_options = { :host => 'www.skunkwerx-performance.com' }
