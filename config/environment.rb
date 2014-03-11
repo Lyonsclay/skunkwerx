3ce87df1101245e7f077a277016d0170f3e71b04
Clay Morton <lyonsclay@yahoo.com>
2014-03-10 23:07:41 -0400

    Remove config.action_mailer from production.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Skunkwerx::Application.initialize!
 
-config.action_mailer.default_url_options = { :host => 'www.skunkwerx-performance.com' }
