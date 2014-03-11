3f0807bba27fa4e01e7ffc6a474cb04e5e3375b2
Clay Morton <lyonsclay@yahoo.com>
2014-03-11 06:53:33 -0400

    Add smtp settings to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,18 @@ Skunkwerx::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'www.skunkwerx-performance.com' }
+    # Action Mailer configuration
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: 'clay.morton',
+    password: '10times10'
+  }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.default_url_options = { :host => 'http://tranquil-headland-2085.herokuapp.com/' }
 end
