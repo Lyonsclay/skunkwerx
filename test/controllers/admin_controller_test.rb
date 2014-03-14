d41fafa7132136d04109a1d95b46b91e9ca49a32
Clay Morton <lyonsclay@yahoo.com>
2014-03-14 05:02:33 -0400

    Add edit action to products controller and test

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
+require 'pry'
+include SessionsHelper
 
 class AdminControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should redirect to sessions#new" do
+    get :index
+    assert_response :redirect
+    assert_redirected_to(controller: "sessions", action: "new")
+  end
+
+
+  test "should get admin/products" do
+    sign_in(Admin.first)
+    get :index
+# binding.pry
+    assert_response :success
+    assert_template :index
+    # assert_response '.menu_bar ul li', 'HOME'
+  end
 end
