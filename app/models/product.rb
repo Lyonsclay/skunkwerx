cfebcbee59af5964fab54ce3039da1d8fd73a5a5
Clay Morton <lyonsclay@yahoo.com>
2014-03-19 17:29:14 -0400

    Implement paperclip with s3 storage for product.image

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,10 +2,24 @@ class Product < ActiveRecord::Base
   validates :name, :description, :quantity, :price, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :name, uniqueness: true
-  validates :image_url, allow_blank: true, format: {
-    with: %r{\.(gif|jpg|png)\Z}i,
-    message: 'must be a URL for GIF, JPG or PNG image.'
-  }
+  # validates :image, allow_blank: true, format: {
+  #   with: %r{\.(gif|jpg|png)\Z}i,
+  #   message: 'must be a URL for GIF, JPG or PNG image.'
+  # }
+
+  # This method associates the attribute ":image" with a file atachment
+  has_attached_file :image,
+    styles: {
+      thumb: '100x100#',
+      square: '200x200#',
+      medium: '300x300#'
+    },
+    bucket: 'skunkwerx',
+    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+
+  # Validate the attached image is image/jpg, image/png, etc
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   # For caching
   def self.latest
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,14 @@ Skunkwerx::Application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.default_url_options = { :host => 'http://tranquil-headland-2085.herokuapp.com/' }
+
+  # Paperclip gem for uploading photos
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: { bucket: ENV['S3_BUCKET_NAME'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    }
+    s3_host_name: 's3-us-west-2.amazonaws.com'
+  }
 end
