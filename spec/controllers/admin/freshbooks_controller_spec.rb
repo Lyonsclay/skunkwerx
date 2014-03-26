74cd680b466d9850459d56ffe44180eea0fa958c
Clay Morton <lyonsclay@yahoo.com>
2014-03-26 16:47:27 -0400

    Add freshbooks_controller_spec

--- spec/controllers/admin/freshbooks_controller_spec.rb
+++ spec/controllers/admin/freshbooks_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
+require 'pry'
 
 describe Admin::FreshbooksController do
+  let(:admin) { create(:admin) }
 
+  describe "GET index" do
+    it "retrieves the index" do
+      get :index
+
+      expect(response).to render_template("index")
+    end
+  end
+
+  describe "Freshbooks :items_sync" do
+    it "should get items from freshbooks" do
+      controller.stub(:current_admin).and_return(true)
+      expect(Product.last).to eq(nil)
+      get :items_sync
+      expect(response).should redirect_to '/admin'
+      expect(Product.last).not_to eq(nil)
+    end
+  end
 end
