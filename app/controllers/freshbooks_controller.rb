595edf1e936bbb9872c9664a8cc8a37fbadfbd25
Clay Morton <lyonsclay@yahoo.com>
2014-04-03 13:06:02 -0400

    Clean up

--- app/controllers/freshbooks_controller.rb
+++
@@ -1,2 +0,0 @@
-class FreshbooksController < ApplicationController
-end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,16 @@
-# require 'pry'
-
 class SessionsController < ApplicationController
   # layout can also receive symbol
   layout 'admin'
 
   def new
-
   end
 
   def create
-# binding.pry
     email = params[:session][:email]
     password = params[:session][:password]
     admin = Admin.find_by(email: email)
     if admin && admin.authenticate(password)
       sign_in(admin)
-# binding.pry
-
       redirect_to '/admin'
     else
       flash[:error] = 'Invalid email/password combination'
@@ -26,7 +20,6 @@ class SessionsController < ApplicationController
 
   def delete
     sign_out
-# binding.pry
     redirect_to '/admin'
   end
 end
