137117f24cf510e98bc0a40dcfa86575016c02f2
Clay Morton <lyonsclay@yahoo.com>
2014-04-09 09:44:04 -0400

    Change loop/method conditionals from = to ==

--- app/controllers/admin/freshbooks_controller.rb
+++ app/controllers/admin/freshbooks_controller.rb
@@ -72,7 +72,7 @@ class Admin::FreshbooksController < ApplicationController
     # Check to insure valid freshbooks api request.
     if params[:system] = "https://skunkwerxperformanceautomotivellc.freshbooks.com"
       # Callback Verify action for all webhook methods;
-      if params[:name] = "callback.verify"
+      if params[:name] == "callback.verify"
         puts "**************** inside callback.verify *************"
         verifier = params[:verifier]
         callback_id = Rails.cache.read "callback_id"
@@ -82,7 +82,7 @@ class Admin::FreshbooksController < ApplicationController
         flash[:notice] = display_response(response)
       end
       # Item Create Callback method creates new product.
-      if params[:name] = "item.create"
+      if params[:name] == "item.create"
         puts "**************** inside item.create ****************"
         puts "******************* params *************************"
         puts params
