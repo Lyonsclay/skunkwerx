fc9c660a6259ba18b01ad6a9f2578d250c9bbde2
Clay Morton <lyonsclay@yahoo.com>
2014-04-15 14:21:35 -0400

    Organize methods in freshbooks_helper.rb

--- app/helpers/admin/freshbooks_helper.rb
+++ app/helpers/admin/freshbooks_helper.rb
@@ -27,6 +27,7 @@ module Admin::FreshbooksHelper
 
 ###############################################################
 # ## Predefined messages to perform Freshbooks api requests; ##
+
   def item_list_message(page)
     "<?xml version=\"1.0\" encoding=\"utf-8\"?>
         <request method=\"item.list\">
@@ -117,21 +118,6 @@ module Admin::FreshbooksHelper
     flash[:notice] = display_response(response_hash)
   end
 
-  # Perform two tests comparing Freshbooks items and Products( web products)
-  # in order to identify discrepancies which indicate webhooks are not
-  # performing correctly.
-  def check_items_against_products(product_items, new_products)
-    message = ""
-    if new_products.any?
-      message = "The following products were newly created; " + new_products.inspect
-    end
-    if product_items.count != Product.count
-      message += "\nBefore syncing with Freshbooks, "
-      message += "there were #{product_items.count} Freshbooks items and #{Product.count} web products"
-    end
-    message
-  end
-
   # Receive item.create request and get product attributes.
   def item_create(object_id)
     puts "**************** inside item.create ****************"
@@ -198,6 +184,27 @@ module Admin::FreshbooksHelper
     end
   end
 
+  #####################################################
+  # ## Support methods that don't make api call; ######
+
+  # Perform two tests comparing Freshbooks items and Products( web products)
+  # in order to identify discrepancies which indicate webhooks are not
+  # performing correctly.
+  def check_items_against_products(product_items, new_products)
+    message = ""
+    if new_products.any?
+      message = "The following products were newly created; " + new_products.inspect
+    end
+    if product_items.count != Product.count
+      message += "\nBefore syncing with Freshbooks, "
+      message += "there were #{product_items.count} Freshbooks items and #{Product.count} web products"
+    end
+    message
+  end
+
+  # If there are products on the Skunkwerx database that
+  # are not on the Freshbooks database they will be removed.
+  # This condition would result if webhooks failed.
   def old_products_delete(product_items)
     item_names = product_items.map { |i| i["name"] }
     product_names = Product.all.map { |p| p.name }
@@ -212,6 +219,7 @@ module Admin::FreshbooksHelper
     end
   end
 
+  # Issue with ["name"] and ["name "] coming from api
   def find_key(key)
     params.keys.detect {|k| k.match(/#{key}/) }
   end
