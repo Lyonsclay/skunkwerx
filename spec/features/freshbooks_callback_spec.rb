2940dbe4b8aca4c384e170510f819093f083f637
Clay Morton <lyonsclay@yahoo.com>
2014-05-09 15:07:33 -0400

    Remove callback_delete method

--- spec/features/freshbooks_callback_spec.rb
+++ spec/features/freshbooks_callback_spec.rb
@@ -1,6 +1,5 @@
 require 'spec_helper'
 require 'capybara/rspec'
-require 'pry'
 
 describe "Freshbooks webhook request", api: true do
   describe "Post webhook_create" do
@@ -18,14 +17,9 @@ describe "Freshbooks webhook request", api: true do
       post admin_webhook_create_path, method: "item.create"
       expect(response.status).to eq(302) # redirect_to '/admin'
       expect(Rails.cache.read "callback_id").to_not eq(nil)
-# binding.pry
-      # action: callback_verify, controller: admin/freshbooks
-      # name=callback.verify&object_id=1&system=https%3A%2F%2F2ndsite.freshbooks.com&user_id=1&verifier=3bPTNcPgbN76QLgKLSR9XdgQJWvhhN4xrT
       post webhooks_path, "name " => "callback.verify", verifier: "3bPTNcPgbN76QLgKLSR9XdgQJWvhhN4xrT", system: "https://skunkwerxperformanceautomotivellc.freshbooks.com"
       expect(response.status).to eq(200)
       expect(flash[:notice]).to_not eq(nil)
-
-      callback_delete("item.create")
     end
   end
 end
