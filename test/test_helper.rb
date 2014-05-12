0e4c15278ea8def41191e9fcdac3f34a2d40d609
Clay Morton <lyonsclay@yahoo.com>
2014-05-12 15:13:40 -0400

    Incorporate Coveralls

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,8 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'capybara/rails'
 require 'capybara/dsl'
+require 'coveralls'
+Coveralls.wear!
 
 class ActionDispatch::IntegrationTest
   # Make the Capybara DSL available in all integration tests
