14fa98a37b4088a111785d0e62d3785f1727a4df
Clay Morton <lyonsclay@yahoo.com>
2014-05-13 12:24:10 -0400

    Create admin in seed file and add to .gitignore

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Admin.create(email: 'clay.morton@gmail.com', password: 'foobar', password_confirmation: 'foobar')
