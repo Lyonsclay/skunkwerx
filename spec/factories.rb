29ca9a29b08473b353322ba57d0ddeb50ab33598
Clay Morton <lyonsclay@yahoo.com>
2014-05-15 16:33:05 -0400

    Add faker gem us in product factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 FactoryGirl.define do
   factory :admin do
     name "Spastic"
@@ -5,6 +7,13 @@ FactoryGirl.define do
     password "foobar"
     password_confirmation "foobar"
     password_reset_token "barfood"
+  end
+
+  factory :product do
+    name Faker::Name.name
+    description "The birdy must be free!"
+    quantity 2
+    unit_cost 23.56
   end
 end
 
