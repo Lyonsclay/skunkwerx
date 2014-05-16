d06bb7d7dab149d0f0a47a120ef0b4fc6e8d555f
Clay Morton <lyonsclay@yahoo.com>
2014-05-15 20:10:58 -0400

    Rake db:migrate line_items and carts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Skunkwerx::Application.routes.draw do
+  resources :line_items
+
+  resources :carts
+
   # get "products/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
