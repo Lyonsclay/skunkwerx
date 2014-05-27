ec61286fec500dfd1af826fa3313ba7e088dbfbe
Clay Morton <lyonsclay@yahoo.com>
2014-05-27 09:53:11 -0400

    Get Rspec line_items and carts controller tests to pass

--- spec/views/line_items/new.html.erb_spec.rb
+++ spec/views/line_items/new.html.erb_spec.rb
@@ -10,11 +10,9 @@ describe "line_items/new" do
 
   it "renders new line_item form" do
     render
-
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form[action=?][method=?]", line_items_path, "post" do
-      assert_select "input#line_item_product[name=?]", "line_item[product]"
-      assert_select "input#line_item_cart[name=?]", "line_item[cart]"
+      assert_select "input#line_item_product_id[name=?]", "line_item[product_id]"
+      assert_select "input#line_item_cart_id[name=?]", "line_item[cart_id]"
     end
   end
 end
