77be96cb4c8984d9412034edec682cf5f1fd121f
Clay Morton <lyonsclay@yahoo.com>
2014-06-22 14:35:53 -0400

    Improve test_webhooks_spec

--- spec/features/test_webhooks_spec.rb
+++ spec/features/test_webhooks_spec.rb
@@ -5,19 +5,47 @@ include Features::WebhooksTestHelpers
 
 describe "Get products from Skunkwerx index" do
 
-  it "Provides array of products" do
-    products = skunkwerx_get_products
-    name = "Goofy-rspec" + SecureRandom.urlsafe_base64(nil, false)[1..5]
-# binding.pry
-    response = skunkwerx_create_product(name)
-    item_id = skunkwerx_get_item_id(name)
-# binding.pry
-    products_new = skunkwerx_get_products
-    expect(products_new.count - products.count).to eq(1)
-    response = skunkwerx_destroy_product(item_id)
-# binding.pry
-    products = products_new
-    products_new = skunkwerx_get_products
-    expect(products_new.count - products.count).to eq(-1)
+  it "Visits Skunkwerx products/index" do
+    get(ENV['SKUNKWERX_URL'] + '/products/index')
+    expect(response.status).to eq(200)
+  end
+
+  before(:all) do
+    @name ||= "Goofy-rspec-" + SecureRandom.urlsafe_base64(nil, false)[1..5]
+  end
+
+  let(:item_id) { skunkwerx_get_item_id }
+
+
+  it "Creates new product" do
+    products = skunkwerx_get_products.count
+    response = skunkwerx_create_product(@name)
+    # Delay until product has been created and callback issues.
+    num = 0
+    until (products + 1 == skunkwerx_get_products.count) || num == 10 do
+      num += 1
+    end
+    products_new = skunkwerx_get_products.count
+    expect(products_new - products).to eq(1)
+  end
+
+  it "Updates new product" do
+    description = "Actually it's not so great."
+    response_hash = skunkwerx_update_product(description)
+binding.pry
+    # Get description from Skunkwerx website.
+    expect(skunkwerx_get_item_description).to eq(description)
+  end
+
+  it "Deletes new product" do
+    products = skunkwerx_get_products.count
+    response = skunkwerx_destroy_product
+    # Delay until product has been deleted and callback issues.
+    num = 0
+    until (products - 1 == skunkwerx_get_products.count) || num == 10 do
+      num += 1
+    end
+    products_new = skunkwerx_get_products.count
+    expect(products_new - products).to eq(-1)
   end
 end
--- spec/support/features/webhooks_test_helpers.rb
+++ spec/support/features/webhooks_test_helpers.rb
@@ -11,20 +11,34 @@ module Features
 
     def skunkwerx_create_product(name)
       product = Product.new
-      product.name = name
+      product.name = @name
       product.description = "It happens to be so great!"
       product.unit_cost = "21.99"
       freshbooks_call(item_create_message(product))
     end
 
-    def skunkwerx_destroy_product(item_id)
+    def item_update_message(description)
+      "<?xml version=\"1.0\" encoding=\"utf-8\"?><request method=\"item.update\"><item><item_id>#{item_id}</item_id><description>#{description}</description></item></request>"
+    end
+
+    def skunkwerx_update_product(description)
+      freshbooks_call(item_update_message(description))
+    end
+
+    def skunkwerx_destroy_product
       freshbooks_call(item_delete_message(item_id))
     end
 
-    def skunkwerx_get_item_id(name)
+    def skunkwerx_get_item_id
       items = get_items
-      items.select { |item| item["name"] == name }
+      items.select { |item| item["name"] == @name }
       items.first["item_id"]
+    end
+
+    def skunkwerx_get_item_description
+      doc = Nokogiri::HTML(open(ENV['SKUNKWERX_URL'] + '/products/index'))
+      elem = doc.search("//dt[contains(text(), \"#{@name}\")]")
+      elem.first.next_element.next_element.content.strip
     end
   end
 end
