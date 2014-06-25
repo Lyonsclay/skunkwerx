7caf86df90f5e684e0fb2c6688f64a1787877076
Clay Morton <lyonsclay@yahoo.com>
2014-06-25 18:20:19 -0400

    Move create model and relations to setup block

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -4,24 +4,20 @@ class OrdersControllerTest < ActionController::TestCase
 
   setup do
     @request.env['HTTP_REFERER'] = products_index_url
-    product = products(:product_one)
+    @product = products(:product_one)
     @cart = Cart.create
     session[:cart_id] = @cart.id
-    # @cart.line_items << LineItem.create(item_id: product.item_id)
+    @cart.line_items << LineItem.create(item_id: @product.item_id)
   end
 
   test "requires item in cart" do
+    @cart.line_items.delete_all
     get :new
     assert_redirected_to request.referer
     assert_equal flash[:notice], 'Your cart is empty'
   end
 
   test "should get new" do
-    item = LineItem.new
-    item.build_cart
-    item.product = products(:product_one)
-    item.save!
-    session[:cart_id] = item.cart.id
     get :new
     assert_response :success
   end
