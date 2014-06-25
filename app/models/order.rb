ad7ffd134db65f3015f722f20a5cfdfd5dd8caa5
Clay Morton <lyonsclay@yahoo.com>
2014-06-25 18:15:46 -0400

    Create Order has_many line_items relationship

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,4 @@
 class Order < ActiveRecord::Base
   PAYMENT_TYPES = ["Check", "Paypal"]
-
+  has_many :line_items, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140623214334) do
+ActiveRecord::Schema.define(version: 20140625144626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,10 +38,12 @@ ActiveRecord::Schema.define(version: 20140623214334) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "quantity",   default: 1
+    t.integer  "order_id"
   end
 
   add_index "line_items", ["cart_id"], name: "index_line_items_on_cart_id", using: :btree
   add_index "line_items", ["item_id"], name: "index_line_items_on_item_id", using: :btree
+  add_index "line_items", ["order_id"], name: "index_line_items_on_order_id", using: :btree
 
   create_table "malone_tunes", force: true do |t|
     t.string   "name"
