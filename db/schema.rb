8f540d526208201d06e5f1ded2ba2d15a3fab274
Clay Morton <lyonsclay@yahoo.com>
2014-07-24 12:08:37 -0400

    Add default value to column years in engines

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140721214949) do
+ActiveRecord::Schema.define(version: 20140723030106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 20140721214949) do
 
   create_table "engines", force: true do |t|
     t.string   "engine"
-    t.string   "years",      array: true
+    t.string   "years",      default: [], array: true
     t.integer  "model_id"
     t.datetime "created_at"
     t.datetime "updated_at"
