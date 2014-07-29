b0da63784f8efa2cd0257f64653c871dfb942162
Clay Morton <lyonsclay@yahoo.com>
2014-07-28 23:03:44 -0400

    Edit spec/support/freshbooks_callbacks.rb

--- spec/support/freshbooks_callbacks.rb
+++ spec/support/freshbooks_callbacks.rb
@@ -1,38 +1,37 @@
 module FreshbooksCallbacks
 
-    def callback_delete_message(callback_id)
-      "<?xml version=\"1.0\" encoding=\"utf-8\"?>
-        <request method=\"callback.delete\">
-          <callback_id>#{callback_id}</callback_id>
-        </request>"
-    end
+  def callback_delete_message(callback_id)
+    "<?xml version=\"1.0\" encoding=\"utf-8\"?>
+      <request method=\"callback.delete\">
+        <callback_id>#{callback_id}</callback_id>
+      </request>"
+  end
 
-    def delete_all_webhooks
-      callback_list = freshbooks_call(callback_list_message)
-      doc = Document.new callback_list.to_xml
-      callback_list.each do |callback|
-        callback_ids = REXML::XPath.match( doc, '//callback-id')
-        callback_ids.map { |e| e.text.to_i }.each do |id|
-          response_hash = freshbooks_call(callback_delete_message(id))
-        end
+  def delete_all_webhooks
+    callback_list = freshbooks_call(callback_list_message)
+    doc = Document.new callback_list.to_xml
+    callback_list.each do |callback|
+      callback_ids = REXML::XPath.match( doc, '//callback-id')
+      callback_ids.map { |e| e.text.to_i }.each do |id|
+        response_hash = freshbooks_call(callback_delete_message(id))
       end
     end
+  end
 
-    def get_callback_verify(method)
-      callback_verify = "0"
-      num = 0
-      # This until loop provides small delay for app at
-      # skunkwerx-performance.com to receive verify callback,
-      # and authenticate verify callback with Freshbooks API.
-      until callback_verify == "1" || num == 10 do
-        num += 1
-        doc = Document.new callbacks_display.to_xml
-        callbacks = REXML::XPath.match(doc, '//callback/event')
-        callback = callbacks.keep_if { |c| c.text == method }
-        callback_verify = callback.first.next_element.text
-      end
-      callback_verify
+  def get_callback_verify(method)
+    callback_verify = "0"
+    num = 0
+    # This until loop provides small delay for app at
+    # skunkwerx-performance.com to receive verify callback,
+    # and authenticate verify callback with Freshbooks API.
+    until callback_verify == "1" || num == 10 do
+      num += 1
+      doc = Document.new callbacks_display.to_xml
+      callbacks = REXML::XPath.match(doc, '//callback/event')
+      callback = callbacks.keep_if { |c| c.text == method }
+      callback_verify = callback.first.next_element.text
     end
+    callback_verify
   end
 
   def get_callbacks_list
