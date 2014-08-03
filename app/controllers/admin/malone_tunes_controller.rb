adf332980c0135ed6f6e832b2d1d15f9cdd340dd
Clay Morton <lyonsclay@yahoo.com>
2014-08-03 17:20:03 -0400

    Move methods from malone_tunes to malone_tunings controller

--- app/controllers/admin/malone_tunes_controller.rb
+++ app/controllers/admin/malone_tunes_controller.rb
@@ -2,29 +2,10 @@ class Admin::MaloneTunesController < ApplicationController
   layout 'admin/application'
   before_filter :authorize
 
-  # List of engines from malonetuning.com.
-  # User selects engine to show associated tunes.
-  # Get '/show'
-  def malone_tuning_index
-    @models = vehicle_models
-  end
-
-  # Presently '/malone_tunes/index_deploy'
   def index
     @malone_tunes = MaloneTune.all
   end
 
-  # Show all tunes for selected engine.
-  # User selects tunes to create as Product and MaloneTune.
-  # Post '/create'
-  def show
-    @model_tunes = model_engine_tunes
-    @model_tunes.pop if @model_tunes.last[:name].empty?
-    @make_model = params[:model][:make]
-    @make_model += params[:model][:model] unless params[:model][:model].nil?
-  end
-
-  #
   def create
 binding.pry
     # @tune_ids = new_malone_tunes_from_params
@@ -32,6 +13,10 @@ binding.pry
 
   def edit
     @malone_tune = MaloneTune.find_by id: params[:id]
+  end
+
+  def new
+    binding.pry
   end
 
   def update
