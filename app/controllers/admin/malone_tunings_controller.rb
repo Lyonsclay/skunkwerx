a09992bbc92a1a4da06b047b8f9aa8d9807dba45
Clay Morton <lyonsclay@yahoo.com>
2014-08-07 16:27:10 -0400

    Store @malone_tunings in session variable

--- app/controllers/admin/malone_tunings_controller.rb
+++ app/controllers/admin/malone_tunings_controller.rb
@@ -7,20 +7,19 @@ class Admin::MaloneTuningsController < ApplicationController
 
   # List of engines from malonetuning.com.
   # User selects engine to show associated tunes.
-  # Get '/show'
   def vehicle_index
     @models = vehicle_models
   end
 
   def index
-
+    @malone_tunings = session[:malone_tunings]
   end
 
   # Show all tunes for selected engine.
   # User selects tunes to create as Product and MaloneTune.
   # Post '/create'
   def create
-    @malone_tunings = vehicle_tunings
+    session[:malone_tunings] = @malone_tunings = vehicle_tunings
     @malone_tunings.pop if @malone_tunings.last[:name].empty?
     @make_model = params[:model][:make]
     @make_model += params[:model][:model] unless params[:model][:model].nil?
