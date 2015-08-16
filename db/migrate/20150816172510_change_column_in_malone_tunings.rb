58fb5590bd9612c62eef898be8bcb2ad1e918fd6
Clay Morton <lyonsclay@yahoo.com>
2015-08-16 13:29:12 -0400

    Changed migration reference to MaloneTuning

--- db/migrate/20150816172510_change_column_in_malone_tunings.rb
+++ db/migrate/20150816172510_change_column_in_malone_tunings.rb
@@ -1,6 +1,6 @@
-class ChangeColumnNameInMaloneTune < ActiveRecord::Migration
+class ChangeColumnInMaloneTunings < ActiveRecord::Migration
   def up
-    MaloneTune.connection.execute("UPDATE malone_tunes SET name=LEFT(name,50)")
+    MaloneTuning.connection.execute("UPDATE malone_tunes SET name=LEFT(name,50)")
     change_column :malone_tunes, :name, :string, limit: 50
   end
 
